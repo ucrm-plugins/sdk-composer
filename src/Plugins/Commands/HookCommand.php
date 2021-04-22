ed17e44310d95a50da776fa0d906ff79736ed807
rspaeth79 <rspaeth@mvqn.net>
2021-04-22 16:44:44 -0700

    Completed command:hook work

--- src/Plugins/Commands/HookCommand.php
+++ src/Plugins/Commands/HookCommand.php
@@ -1,5 +1,4 @@
-<?php /** @noinspection PhpUnused */
-/** @noinspection PhpUnusedParameterInspection */
+<?php /** @noinspection PhpUnused, PhpUnusedParameterInspection */
 declare( strict_types=1 );
 
 namespace UCRM\Composer\Plugins\Commands;
@@ -14,22 +13,19 @@ use Symfony\Component\Console\Style\SymfonyStyle;
 
 class HookCommand extends BaseCommand
 {
-
-
-
     /**
-     * Configures this plugin for use with the composer system.
+     * Configures this command for use with the composer system.
      */
     protected function configure()
     {
         $this->setName( "hook" );
 
-        $this->addArgument( "hook", InputArgument::REQUIRED, "The hook to execute (install|update|configure|enable|disable|remove)" );
+        $this->addArgument( "hook", InputArgument::REQUIRED,
+            "The hook to execute (install|update|configure|enable|disable|remove)" );
     }
 
-
     /**
-     * Handles validation of the project prior to the plugin's execution.
+     * Executes when this command is used.
      *
      * @param InputInterface  $input  Input from the composer system.
      * @param OutputInterface $output Output to the composer system.
@@ -59,6 +55,7 @@ class HookCommand extends BaseCommand
 
         $io->section("Simulating '$hook' hook...");
 
+        /** @noinspection PhpIncludeInspection */
         include $file;
 
     }
